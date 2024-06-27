20191a38aaf33f01ff91272746a3d97adef5fe27
BRAJPATIDAR03 <121357639+BRAJPATIDAR03@users.noreply.github.com>
2024-06-27 12:14:29 +0000

    done some changes

---
+++ 01_basics/01_vairable.js
@@ -0,0 +1,15 @@
+const account_id = 1668621
+let account_idemail = "braj@fdudsf.com"
+var account_id_password="389e3e8h"
+accountcity = "khargone"
+let accountstate;
+//not use var
+
+
+account_idemail = "2uefg.com"
+account_id_password ="fjdggfdk"
+accountcity= "bhopal"
+
+
+console.log(account_id);
+console.table([account_id,accountstate,account_idemail,account_id_password,accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict";   //treat as latest js
+
+//alert(3+3) we are using node js not browser
+
+console.log(3+3);
